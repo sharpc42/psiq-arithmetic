02439598ef66577ff9e38d7376b45dfa42fb2d3d
Chris Sharp <sharpc42@uw.edu>
2026-04-20 15:45:14 +0000

    Fixed target and control register the same error

--- wangrca_qubrick.py
+++ wangrca_qubrick.py
@@ -9,24 +9,31 @@ class WangRCA(Qubrick):
 
         # initialize auxiliary qubits
         aux = self.alloc_temp_qreg(
-            num_qubits+1, 
+            num_qubits, 
             "aux",
+            release_after_compute=True,
+        )
+        c_0 = self.alloc_temp_qreg(
+            1,
+            "c_0",
             release_after_compute=True,
         )
 
         # initial s1 layer
         aux[0].x(cond=a[0] | b[0])
-        a[0].x(cond=aux[0] ^ a[0])   # a_0 -> c_1
+        c_0.x(cond=a[0])
+        a[0].x(cond=aux[0] ^ c_0)        # a_0 -> c_1
+        aux[0].write(0)
         # iterate through layers
         for idx in range(num_qubits):
             # s2 layer
-            a[idx].x(cond=aux[idx+1])    # c_j -> s_j
-            b[idx].x(cond=aux[idx+1])    # b_j -> a_j
+            a[idx].x(cond=aux[idx])    # c_j -> s_j
+            b[idx].x(cond=aux[idx])    # b_j -> a_j
             # s1 layer
-            aux[idx+2].x(cond=b[idx+1])
+            aux[idx+1].x(cond=b[idx+1])
             a[idx].x(cond=a[idx+1])
             b[idx+1].x(cond=a[idx+1])
             a[idx+1].x(cond=a[idx] | b[idx+1])
         # final s2 layer
-        a[-2].x(cond=aux[-1])
-        b[-2].x(cond=aux[-1])
+        a[-1].x(cond=aux[-1])
+        b[-1].x(cond=aux[-1])
