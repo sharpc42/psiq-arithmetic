4796cbc71e44e2007e1590241824c8a49028a249
Chris Sharp <sharpc42@uw.edu>
2026-04-20 15:25:47 +0000

    Initial commit for Wang RCA qubrick

---
+++ wangrca_qubrick.py
@@ -0,0 +1,34 @@
+from psiqworkbench import Qubrick
+
+class WangRCA(Qubrick):
+
+    def __init__(self, name=None, **kwargs):
+        super().__init__(name, never_uncompute=True, **kwargs)
+
+    def _compute(self, a, b, num_qubits=1):
+
+        # initialize QPU and auxiliary qubits
+        aux = self.alloc_temp_qreg(
+            num_qubits+1, 
+            "aux",
+            release_after_compute=True,
+        )
+
+        # initial s1 layer
+        aux[0].x(cond=a[0] | b[0])
+        a[0].x(cond=aux[0] ^ a[0])   # a_0 -> c_1
+        # iterate through layers
+        for idx in range(num_qubits):
+            # s2 layer
+            a[idx].x(cond=aux[idx+1])    # c_j -> s_j
+            b[idx].x(cond=aux[idx+1])    # b_j -> a_j
+            # s1 layer
+            aux[idx+2].x(cond=b[idx+1])
+            a[idx].x(cond=a[idx+1])
+            b[idx+1].x(cond=a[idx+1])
+            a[idx+1].x(cond=a[idx] | b[idx+1])
+        # final s2 layer
+        a[-2].x(cond=aux[-1])
+        b[-2].x(cond=aux[-1])
+
+        aux.release()
