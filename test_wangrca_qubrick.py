01629c91c6d396b0e9f594170afd118e1b87606c
Chris Sharp <sharpc42@uw.edu>
2026-05-03 05:57:18 +0000

    Made sure lhs is uncomputed correctly

--- test_wangrca_qubrick.py
+++ test_wangrca_qubrick.py
@@ -4,11 +4,12 @@ from psiqworkbench.filter_presets import BIT_DEFAULT
 
 def test_adder_one_value(num_qubits=4, a_val=5, b_val=11):
     wrca = wangrca_qubrick.WangAdd()
+    num_qubits = (a_val + b_val).bit_length()
     qpu = QPU(num_qubits=3*num_qubits+1, filters=BIT_DEFAULT)
     qpu.enable_qubit_allocation_debugging()
-    a = Qubits(num_qubits + 1, "a", qpu=qpu)
+    a = Qubits(num_qubits, "a", qpu=qpu)
     b = Qubits(num_qubits, "b", qpu=qpu)
-    a.write(a_val << 1)  # shift left to make room for carry bit
+    a.write(a_val)
     b.write(b_val)
     wrca.compute(lhs=a, rhs=b, num_qubits=num_qubits)
     result = wrca.get_result_qreg()
@@ -18,18 +19,33 @@ def test_adder_one_value(num_qubits=4, a_val=5, b_val=11):
 def test_adder_fifty_values():
     for i in range(1,50):
         for j in range(1,50):
-            num_qubits = max(i.bit_length(), j.bit_length())
+            num_qubits = max(i.bit_length(), j.bit_length(), (i+j).bit_length())
             test_adder_one_value(num_qubits=num_qubits, a_val=i, b_val=j)
 
 def test_rhs_consistency(num_qubits=4, a_val=5, b_val=11):
     wrca = wangrca_qubrick.WangAdd()
+    num_qubits = (a_val + b_val).bit_length()
     qpu = QPU(num_qubits=3*num_qubits+1, filters=BIT_DEFAULT)
     qpu.enable_qubit_allocation_debugging()
-    a = Qubits(num_qubits + 1, "a", qpu=qpu)
+    a = Qubits(num_qubits, "a", qpu=qpu)
     b = Qubits(num_qubits, "b", qpu=qpu)
-    a.write(a_val << 1)  # shift left to make room for carry bit
+    a.write(a_val)
     b.write(b_val)
     wrca.compute(lhs=a, rhs=b, num_qubits=num_qubits)
     wrca.uncompute()
     b_result = b.read()
     assert b_result == b_val, f"rhs was {b_val} before sum = lhs + rhs, now it is {b_result}"
+
+def test_lhs_consistency(num_qubits=4, a_val=5, b_val=11):
+    wrca = wangrca_qubrick.WangAdd()
+    num_qubits = (a_val + b_val).bit_length()
+    qpu = QPU(num_qubits=3*num_qubits+1, filters=BIT_DEFAULT)
+    qpu.enable_qubit_allocation_debugging()
+    a = Qubits(num_qubits, "a", qpu=qpu)
+    b = Qubits(num_qubits, "b", qpu=qpu)
+    a.write(a_val)
+    b.write(b_val)
+    wrca.compute(lhs=a, rhs=b, num_qubits=num_qubits)
+    wrca.uncompute()
+    a_result = a.read()
+    assert a_result == a_val, f"lhs was {a_val} before lhs += rhs, now it is {a_result}"
