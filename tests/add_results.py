4c9fc6252ac622201a1a340e32e88e23a1c55d42
Chris Sharp <sharpc42@uw.edu>
2026-05-03 23:47:10 +0000

    Extended upper range of many values

--- tests/add_results.py
+++ tests/add_results.py
@@ -13,9 +13,9 @@ class AddResults:
         b.write(b_val)
         with self.adder.computed(lhs=a, rhs=b, num_qubits=num_qubits) as result:
             assert result == a_val + b_val, f"Expected {a_val + b_val}, got {result}"
-    def test_adder_fifty_values(self):
-        for i in range(1,50):
-            for j in range(0,50):
+    def test_adder_many_values(self):
+        for i in range(1,150):
+            for j in range(0,150):
                 print(f"Fifty vals - Testing {i=} and {j=}")
                 num_qubits = max(i.bit_length(), j.bit_length(), (i+j).bit_length())
                 self.test_adder_one_value(num_qubits=num_qubits, a_val=i, b_val=j)
